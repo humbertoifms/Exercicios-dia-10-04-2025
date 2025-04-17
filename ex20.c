f1e33e5ce7d6f8e126abb4bb5a30ff0072e7e54c
humberto2006 <humberto.lemos@estudante.ifms.edu.br>
2025-04-17 07:53:12 -0400

    ex20 corrigido

--- ex20.c
+++ ex20.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  int n1;
+  int n1, temp;
   printf("Quantos caracteres você quer inserir?: ");
   scanf(" %d", &n1);
   char letras[n1];
@@ -14,8 +14,6 @@ int main() {
   for (int i = 0; i < tamLetras; i++){
     printf("Insira o °%d caractere: ", i + 1);
     scanf(" %c", &letras[i]);
-  }
-  for (int i = 0; i < tamLetras; i++){
     for (int j = 0; j < tamAlfabeto; j++){
       if (letras[i] == alfabeto[j]){
         letras[i] = j + 1;
@@ -23,8 +21,7 @@ int main() {
       alfabetoNum[j] = j + 1;
     }
   }
-  int temp;
-  for (int i = 0; i < tamLetras - 1; i++){
+  for (int i = 0; i < tamLetras; i++){
     for (int j = 0; j < tamLetras - 1; j++){
       if (letras[j] > letras[j + 1]){
         temp = letras[j];
